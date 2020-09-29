f4467c8daf30b8c50a78541227ad45935085dc61
redledog <godelder1585@gmail.com>
2020-09-30 01:50:57 +0900

    TodoTemplate js 및 scss 작성 중

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import TodoTemplate from './components/TodoTemplate';
+
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
-      <div>Hello World!</div>
+      <TodoTemplate>Hello</TodoTemplate>
     </div>
   );
 }
--- src/components/TodoTemplate.js
+++ src/components/TodoTemplate.js
@@ -1,1 +1,13 @@
 // 투두 리스트의 레이아웃을 설정하는 컴포넌트
+import React from 'react';
+import './TodoTemplate.scss';
+
+function TodoTemplate({children}) {
+    return (
+        <div className='TodoTemplate'>
+            {children}
+        </div>
+    );
+}
+
+export default TodoTemplate;
