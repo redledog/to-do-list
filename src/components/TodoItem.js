daa6c75b7882c42095c42541fbdce891c9d5443c
redledog <godelder1585@gmail.com>
2020-09-30 22:43:13 +0900

    TodoItem js scss 작성 중 Readme.md 새로 생성

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,1 +1,19 @@
 // 각 할일에 대한 정보를 보여주는 컴포넌트, 할일 완료 여부를 체크하거나 제거할 수 있는 기능이 있음
+import React from 'react';
+import './TodoItem.scss';
+import { MdDone, MdDelete } from 'react-icons/md';
+import classNames from 'classnames';
+
+function TodoItem({ id, done, text }) {
+    return (
+        <div className="TodoItem">
+            <div className={classNames("CheckCircle", {done})}>{done && <MdDone />}</div>
+            <div className={classNames("Text", {done})}>{text}</div>
+            <div className="Remove">
+                <MdDelete />
+            </div>
+        </div>
+    );
+}
+
+export default TodoItem;
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,11 +1,15 @@
 // 할일에 대한 정보가 들어있는 컴포넌트 여러개의 TodoItem을 여기서 렌더링
 import React from 'react';
 import './TodoList.scss';
+import TodoItem from './TodoItem';
 
 function TodoList() {
     return (
         <div className="TodoList">
-            TodoList
+            <TodoItem text="할일1" done={true} />
+            <TodoItem text="할일2" done={true} />
+            <TodoItem text="할일3" done={false} />
+            <TodoItem text="할일4" done={false} />
         </div>
     );
 }
